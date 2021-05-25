74f995f096ffee40dd1b8a2c476981d112af6ec7
Egor Savintsev <esavintsev@fun.co>
2021-05-25 13:42:42 +0300

    temp layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 import useSWR from 'swr';
 import { fetchTasks } from './fetchers';
-// import logo from './logo.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -11,10 +10,23 @@ function App() {
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
 
-  console.log({data});
-
   return (
-    <div className="App">123
+    <div className="container py-3">
+      <h1 className="h6">Anyway Labs Test Project</h1>
+
+      <p>Just some good deeds</p>
+
+      <div className="row align-items-start gx-3">
+        <div className="col">
+          <div className="p-3 border bg-light rounded">Custom column padding</div>
+        </div>
+        <div className="col">
+          <div className="p-3 border bg-light rounded">Custom column padding</div>
+        </div>
+        <div className="col">
+          <div className="p-3 border bg-light rounded">Custom column padding</div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 import { makeServer } from './mirage/server';
 
 const environment = process.env.NODE_ENV;
@@ -15,4 +14,4 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
